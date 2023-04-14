80eaf044eb783ab281750ad9884cd18c69b3e0e5
Andrii Slabkyi <aslabkyi@gmail.com>
2023-04-14 15:48:34 +0300

    add lazy loading, .env

---
+++ src/API.js
@@ -0,0 +1,15 @@
+// import axios from 'axios';
+
+// const instance = axios.create({
+//   baseURL: process.env.REACT_APP_BASE_URL,
+// });
+
+// const setAuthHeader = token => {
+//   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+// };
+
+// const clearAuthHeader = () => {
+//   axios.defaults.headers.common.Authorization = '';
+// };
+
+// Верхнє закоментував щоб не було попереджень в консолі vsCode (кому треба буде розкоментуєте)
