2a003739059ab94dcb3c6546fc52c7b8e2e61c7c
Ivan Zarichnyi <91795849+Pug10rd@users.noreply.github.com>
2023-04-16 20:59:33 +0200

    Merge branch 'dev' of https://github.com/ASlabkyi/goit_react_team_project into DiaryList

--- src/components/DiaryList/DiaryList.styled.jsx
+++ src/components/DiaryList/DiaryList.styled.jsx
@@ -1,12 +1,18 @@
 import styled from "styled-components";
 
+export const Box = styled.div`
+  position: relative;
+`
+
+
 export const List = styled.ul`
   //background-color: red;
   width: 500px;
-  height: 270px;
-  position: absolute;
+  height: 305px;
+  position: relative;
   overflow: scroll;
   overflow-x: hidden;
+  z-index: 2;
 
   ::-webkit-scrollbar {
     width: 6px;
@@ -14,8 +20,26 @@ export const List = styled.ul`
   }
   ::-webkit-scrollbar-track {
     background: #f0f1f3;
+    box-shadow: none;
   }
   ::-webkit-scrollbar-thumb {
     background-color: #264061;
+    box-shadow: none;
   }
+`;
+export const BlurBlock = styled.div`
+  height: 30px;
+  width: 500px;
+  /* filter: blur(4px); */
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  z-index: 3;
+  /* background-color: white;
+  opacity: 0.3; */
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 0.5) 0%,
+    #ffffff 42.19%
+  );
 `;
