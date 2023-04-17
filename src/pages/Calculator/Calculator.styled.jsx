5d475c50223a620d3e76c9022aad67b9c1438359
Andrii Slabkyi <aslabkyi@gmail.com>
2023-04-17 14:38:25 +0300

    1

--- src/pages/Calculator/Calculator.styled.jsx
+++ src/pages/Calculator/Calculator.styled.jsx
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
 
 export const CalculatorWrapper = styled.div`
+  /* @media (min-width: 320px) {
+    margin: 0 auto;
+  }
+
   display: flex;
   flex-direction: column;
   justify-content: center;
-  margin: 0 auto;
-  width: 100%;
+  max-width: 100%;
+
   @media (min-width: 1280px) {
     flex-direction: row;
-  }
+  } */
 `;
