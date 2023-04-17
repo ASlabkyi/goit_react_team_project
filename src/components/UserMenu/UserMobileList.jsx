29050894fff85c3e8a1ac12c31a2880fbd3ce494
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-04-17 11:41:26 +0200

    +

--- src/components/UserMenu/UserMobileList.jsx
+++ src/components/UserMenu/UserMobileList.jsx
@@ -3,20 +3,10 @@ import { LinkMobile, Mobilelist } from './UserMenu.styled';
 export const MobileList = ({ onCloseOpen }) => {
   return (
     <Mobilelist>
-      <LinkMobile
-        to="/diary"
-        onClick={e => {
-          onCloseOpen();
-        }}
-      >
+      <LinkMobile to="/diary" onClick={onCloseOpen}>
         Diary
       </LinkMobile>
-      <LinkMobile
-        to="/calculator"
-        onClick={() => {
-          onCloseOpen();
-        }}
-      >
+      <LinkMobile to="/calculator" onClick={onCloseOpen}>
         Calculator
       </LinkMobile>
     </Mobilelist>
