b81ca2dfa1ad0007c7aefd638f75a020dfca38c1
Andrii Slabkyi <aslabkyi@gmail.com>
2023-04-18 22:32:46 +0300

    132

--- src/components/PrivateRout/PrivateRoute.jsx
+++ src/components/PrivateRout/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import { selectIsLoggedIn, selectIsRefreshing } from 'redux/auth/selectors';
 export const PrivateRout = ({ component: Component, redirectTo = '/' }) => {
   const isLogin = useSelector(selectIsLoggedIn);
   const isRefreshing = useSelector(selectIsRefreshing);
-  return !isLogin || !isRefreshing ? <Navigate to={redirectTo} /> : Component;
+  return !isLogin && !isRefreshing ? <Navigate to={redirectTo} /> : Component;
   //   const isRefreshing = useSelector(selectIsRefreshing);
   // return !isLogin ? <Navigate to={redirectTo} /> : Component;
 };
